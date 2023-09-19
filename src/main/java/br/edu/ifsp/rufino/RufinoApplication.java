3154dd5f9d9ed97a3da784f2703bf2855b0d5b66
Gabriel <gabriel.rufino@biotronica.tech>
2023-09-19 08:31:27 -0300

    initial commit

---
+++ src/main/java/br/edu/ifsp/rufino/RufinoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.rufino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RufinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RufinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/rufino/RufinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.rufino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RufinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
